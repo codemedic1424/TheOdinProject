ebc3770a8219ae128c068d8a5b9c1af4243177dd
codemedic1424 <sunnier.stopper.0x@icloud.com>
2024-10-30 21:52:16 -0400

    Playground repo creation

---
+++ JS Playground/README.md
@@ -0,0 +1,7 @@
+<!-- This is a playground/sandbox for JS - local only.  -->
+
+
+<!-- let is a variable that can be reassigned -->
+
+<!-- const is a variable that CANNOT be reassigned -->
+
---
+++ JS Playground/script.js
@@ -0,0 +1,43 @@
+console.log("Hello, World! This is the beginning of something special!")
+
+let name    = 'John';
+let surname = 'Doe';
+
+console.log(name);
+console.log(surname);
+
+let age = 11; 
+console.log(age);
+
+age = 54;
+
+console.log(age);
+
+
+
+const pi = 3.14;
+
+console.log(pi);
+
+console.log((3+2) - 76 * (1+1));
+
+console.log(23+97);
+
+console.log((4+6+9)/77);
+
+let a = 10
+
+console.log(a)
+
+console.log(9*a);
+
+let b = 7 * a;
+
+console.log(b);
+
+
+const max = 57;
+let actual = max - 13;
+let percentage = actual / max;
+
+console.log(percentage);
