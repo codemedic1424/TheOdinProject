fd6d1fd990ebb4b2103e73c5c83028add20dafb4
codemedic1424 <sunnier.stopper.0x@icloud.com>
2024-11-05 11:50:49 -0500

    Further study

--- JS Playground/script.js
+++ JS Playground/script.js
@@ -1,43 +1,82 @@
-console.log("Hello, World! This is the beginning of something special!")
+// // console.log("Hello, World! This is the beginning of something special!")
 
-let name    = 'John';
-let surname = 'Doe';
+// // let name    = 'John';
+// // let surname = 'Doe';
 
-console.log(name);
-console.log(surname);
+// // console.log(name);
+// // console.log(surname);
 
-let age = 11; 
-console.log(age);
+// // let age = 11; 
+// // console.log(age);
 
-age = 54;
+// // age = 54;
 
-console.log(age);
-
-
-
-const pi = 3.14;
-
-console.log(pi);
-
-console.log((3+2) - 76 * (1+1));
-
-console.log(23+97);
-
-console.log((4+6+9)/77);
-
-let a = 10
-
-console.log(a)
-
-console.log(9*a);
-
-let b = 7 * a;
-
-console.log(b);
+// // console.log(age);
 
 
-const max = 57;
-let actual = max - 13;
-let percentage = actual / max;
 
-console.log(percentage);
+// // const pi = 3.14;
+
+// // console.log(pi);
+
+// // console.log((3+2) - 76 * (1+1));
+
+// // console.log(23+97);
+
+// // console.log((4+6+9)/77);
+
+// // let a = 10
+
+// // console.log(a)
+
+// // console.log(9*a);
+
+// // let b = 7 * a;
+
+// // console.log(b);
+
+
+// // const max = 57;
+// // let actual = max - 13;
+// // let percentage = actual / max;
+
+// // console.log(percentage);
+
+// // javscript.info/ifelse Tasks
+
+// let value = prompt('Type a number', 0);
+
+// if (value > 0) {
+//     alert(1);
+// } else if (value < 0) {
+//     alert(-1);
+// } else {
+//     alert(0);
+// }
+
+// let result = (a+b<4) ? "below" : "Over";
+
+// let age = prompt('age?', 18);
+// _____________________________________________________
+// // if (age < 3) {
+// //     message = 'Hi, baby!';
+// //   } else if (age < 18) {
+// //     message = 'Hello!';
+// //   } else if (age < 100) {
+// //     message = 'Greetings!';
+// //   } else {
+// //     message = 'What an unusual age!';
+// //   }
+
+// // let message = (age < 3) ? 'Hi, baby!' :
+// //   (age < 18) ? 'Hello!' :
+// //   (age < 100) ? 'Greetings!' :
+// //   'What an unusual age!';
+
+// // alert( message );
+
+// let message = (login == 'Employee') ? 'Hello':
+//               (login == 'Director') ? 'Greetings' :
+//               (login == ' ') ? 'No Login' :
+//               ' '; 
+
