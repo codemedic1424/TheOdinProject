0398fba07eadb3f41a61c3d1dc2d0d41ce52d608
codemedic1424 <sunnier.stopper.0x@icloud.com>
2024-11-27 13:00:45 -0500

    project update

--- Foundations-JS/script.js
+++ Foundations-JS/script.js
@@ -9,6 +9,12 @@ console.log(surname)
 console.log(fullName)
 
 const pi = 3.14
-pi = 10
+// pi = 10
 // Will throw an error. Const cant not be changed.
 console.log(pi) 
+
+console.log(23 + 97)
+console.log((4+6+9)/77) 
+
+// javascript.info Variables article
+
