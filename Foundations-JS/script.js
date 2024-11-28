e15221a36f36dfff4c98035fce08066dd6644ba4
codemedic1424 <sunnier.stopper.0x@icloud.com>
2024-11-28 16:28:47 -0500

    Notes update

--- Foundations-JS/script.js
+++ Foundations-JS/script.js
@@ -126,4 +126,82 @@ if (a < b) {
   console.log(`${a} and ${b} are equal.`);
 }
 
+// String primitives and String objects
 
+const strPrim = "foo"; // A literal is a string primitive
+const strPrim2 = String(1); // Coerced into the string primitive "1"
+const strPrim3 = String(true); // Coerced into the string primitive "true"
+const strObj = new String(strPrim); // String with new returns a string wrapper object.
+
+console.log(typeof strPrim); // "string"
+console.log(typeof strPrim2); // "string"
+console.log(typeof strPrim3); // "string"
+console.log(typeof strObj); // "object"
+
+// Conditionals
+
+// JS Conditional statements:
+/* Use if to specify a block of code to be executed, if a specified condition is true
+Use else to specify a block of code to be executed, if the same condition is false
+Use else if to specify a new condition to test, if the first condition is false
+Use switch to specify many alternative blocks of code to be executed */
+
+// MDN Condition Lesson (Great examples for future website features)
+
+// covers the basic if...else
+// Switch Statements - 
+
+/* switch (expression) {
+  case choice1:
+    // run this code
+    break;
+
+  case choice2:
+    // run this code instead
+    break;
+
+  // include as many cases as you like
+
+  default:
+    // actually, just run this code
+    break;
+}
+ */
+// Practical Example:
+/* const select = document.querySelector("select");
+const para = document.querySelector("p");
+
+select.addEventListener("change", setWeather);
+
+function setWeather() {
+  const choice = select.value;
+
+  switch (choice) {
+    case "sunny":
+      para.textContent =
+        "It is nice and sunny outside today. Wear shorts! Go to the beach, or the park, and get an ice cream.";
+      break;
+    case "rainy":
+      para.textContent =
+        "Rain is falling outside; take a rain coat and an umbrella, and don't stay out for too long.";
+      break;
+    case "snowing":
+      para.textContent =
+        "The snow is coming down — it is freezing! Best to stay in with a cup of hot chocolate, or go build a snowman.";
+      break;
+    case "overcast":
+      para.textContent =
+        "It isn't raining, but the sky is grey and gloomy; it could turn any minute, so take a rain coat just in case.";
+      break;
+    default:
+      para.textContent = "";
+  }
+}
+ */
+// Ternary operator
+
+// Like an if...else statement, but shorter code
+// condition ? run this code : run this code instead
+
+// Regular Expressions (REGEX)
+ 
