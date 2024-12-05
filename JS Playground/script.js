94b8f4449006baebd74f2b5be686627bfda92053
codemedic1424 <sunnier.stopper.0x@icloud.com>
2024-12-05 00:50:14 -0500

    notes complete

--- JS Playground/script.js
+++ JS Playground/script.js
@@ -40,4 +40,26 @@
 // // let actual = max - 13;
 // // let percentage = actual / max;
 
-console.log(percentage);
+// console.log(percentage);
+
+/* ---------> JS Array Method Examples <----------*/
+
+let numbers = [1,2,3,4,5,6];
+
+numbers.forEach((item) => {console.log(item)}); //prints each element into the console.
+
+const three = numbers.map((item) => '3'); //prints each element as a three for the array.length.
+const doubleItem = numbers.map((item) => item + item); //returns each element 'doubled' in the array.
+
+const evens = numbers.filter((item) => item.type === Number); //places items that meet filter parameter in new array 'evens'.
+
+const newArray = Array.concat (Value/Array); //merges arrays or values into a new array.
+
+const newArray1 = array.find((item) => item.attribute  === 'parameter'); // creates a new array with items that meets the parameter. If no items satisfy parameter - return undefined.
+
+const itemIndex = array.findIndex ((item) => item.attribute === 'parameter'); // returns new array with index of items that meet parameter. If index is negative - no items exist with parameter.
+
+const itemIndex2 = array.indexOf(item, startIndex); //returns the index of first occurring item. if startIndex is blank, starts at 0. 
+//const lastItemIndex = array.lastIndexof(item, Startindex); same as above, just begins at end of array.
+
+
